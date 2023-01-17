5394d6e9a5f4ad0e55f5182dd3da6a2e14a35ced
Aleksei Viunik <vjunik88@gmail.com>
2023-01-17 14:02:26 +0300

    Unnecessary files have been deleted. Readme has been upgraded

--- src/test.py
+++
@@ -1,9 +0,0 @@
-from os.path import join, abspath
-
-tpath = join('.', 'Data', 'sudoky3.txt')
-print(tpath)
-tpath = abspath(tpath)
-print(tpath)
-with open(tpath, 'rt', encoding = 'utf-8') as src:
-    for line in src:
-        print(line)
